b9fff74cbc53c10ab26f0c9759b0e7225764b54a
Wu <fewu@38f9d34e1390.ant.amazon.com>
2019-05-13 17:08:38 -0700

    change mask head function out of the loop

--- MaskRCNN/model/generalized_rcnn.py
+++ MaskRCNN/model/generalized_rcnn.py
@@ -424,16 +424,17 @@ class ResNetFPNModel(DetectionModel):
                 roi_feature_maskrcnn = multilevel_roi_align(
                         features[:4], proposal_fg_boxes, 14,
                         name_scope='multilevel_roi_align_mask')
-                '''
+
                 mask_logits = maskrcnn_head_func(
                         'maskrcnn', roi_feature_maskrcnn, cfg.DATA.NUM_CATEGORY, fp16=self.fp16)   # Num_fg_boxes x num_category x (H_roi_mask*2) x (W_roi_mask*2)
-                '''
+
                 all_mask_losses = []
                 for i in range(cfg.TRAIN.BATCH_SIZE_PER_GPU):
                     single_image_gt_count = prepadding_gt_counts[i] # 1-D Num_GT_boxes_current_image
                     single_image_gt_masks = gt_masks[i, :single_image_gt_count, :, :] # Num_GT_boxes_current_image x H_gtmask x W_gtmask (maybe? might have length 1 dim at beginning)
                     single_image_fg_indices = tf.squeeze(tf.where(tf.equal(proposal_fg_boxes[:, 0], i)), axis=1) # 1-D Num_fg_boxes_current_image
-                    single_image_roi_feature_maskrcnn =  tf.gather(roi_feature_maskrcnn, single_image_fg_indices)
+                    #single_image_roi_feature_maskrcnn =  tf.gather(roi_feature_maskrcnn, single_image_fg_indices)
+                    single_image_mask_logits = tf.gather(mask_logits, single_image_fg_indices)
                     single_image_fg_boxes = tf.gather(proposal_fg_boxes, single_image_fg_indices)[:, 1:] # Num_fg_boxes_current_image x 4
                     single_image_fg_labels = tf.gather(proposal_fg_labels, single_image_fg_indices) # 1-D Num_fg_boxes_current_image
                     single_image_fg_inds_wrt_gt = proposal_gt_id_for_each_fg[i] # 1-D Num_fg_boxes_current_image
@@ -445,8 +446,8 @@ class ResNetFPNModel(DetectionModel):
                     #        single_image_feature, single_image_proposal_fg_boxes, 14,
                     #        name_scope='multilevel_roi_align_mask_{}'.format(i))
 
-                    single_image_mask_logits = maskrcnn_head_func(
-                            'maskrcnn', single_image_roi_feature_maskrcnn, cfg.DATA.NUM_CATEGORY, fp16=self.fp16)
+                    #single_image_mask_logits = maskrcnn_head_func(
+                    #        'maskrcnn', single_image_roi_feature_maskrcnn, cfg.DATA.NUM_CATEGORY, fp16=self.fp16)
 
                     single_image_gt_masks = tf.expand_dims(single_image_gt_masks, axis=1) # Num_GT_boxes_current_image x 1 x H_gtmask x W_gtmask
                     # single_image_target_masks_for_fg: Num_fg_boxes_current_image x 1 x (H_roi_mask*2) x (W_roi_mask*2)
