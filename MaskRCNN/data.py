980c56ea99cf33c8da42d66814f6ddacaa721522
Wu <fewu@38f9d34e1390.ant.amazon.com>
2019-05-15 11:29:36 -0700

    minor fix

--- MaskRCNN/data.py
+++ MaskRCNN/data.py
@@ -643,8 +643,8 @@ def get_batch_train_dataflow(batch_size):
             max_height = max([shp[0] for shp in padding_shapes])
             max_width = max([shp[1] for shp in padding_shapes])
         elif cfg.PREPROC.PADDING_ALL:
-            max_height = cfg.PREPROC.MAX_SIZE
-            max_width = cfg.PREPROC.MAX_SIZE
+            max_height = float(cfg.PREPROC.MAX_SIZE)
+            max_width = float(cfg.PREPROC.MAX_SIZE)
         else:
             image_dims = [d["images"].shape for d in datapoint_list]
             heights = [dim[0] for dim in image_dims]
